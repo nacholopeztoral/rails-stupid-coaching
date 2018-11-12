2a0b2c5c886f7aeac8edd460353fd5ef12f30de0
nacho lopez <nachete-1995@hotmail.com>
2018-11-12 17:30:03 +0100

    pushed first exercise

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,20 @@
 class QuestionsController < ApplicationController
+
   def ask
+    # @answer = params[:answer]
+    # answer(@answer)
   end
 
+
   def answer
+    @answer = params[:answer]
+    if @answer.include?("?")
+      @ans = " Go to work"
+    elsif @answer.include?(" work")
+      @ans = " Great"
+    else
+      @ans = " Nonsense"
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'questions/ask'
-  get 'questions/answer'
+  # get 'questions/ask', to: 'questions#ask', as: :ask
+
+  get 'questions/ask', to: 'questions#ask', as: :ask
+#
+  post "questions/answer", to: 'questions#answer'
+
+  get 'questions/answer', to: 'questions#answer', as: :answer
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
